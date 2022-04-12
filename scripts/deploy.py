4eeb9303e4d5e362dfcb98f6666b9d8d58b53180
chench53 <chench53.logic@gmail.com>
2022-04-13 00:02:04 +0800

    add Governor

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,18 +1,33 @@
-from brownie import AbiToken, config, network
+from tkinter.messagebox import NO
+from brownie import (
+    AbiToken, 
+    AbiMedal, 
+    AbiDao, 
+    TimeLock,
+    config, 
+    network
+)
 from web3 import Web3
 
 from .tools import get_account
 
-def deploy():
+
+def deplopy_contract(contact_container, *args):
     account = get_account()
-    token = AbiToken.deploy(
-        {
-            "from": account
-        },
+    # args = args or []
+    print(contact_container._name, args)
+    contract = contact_container.deploy(*args, {"from": account}, 
         publish_source = config["networks"][network.show_active()].get("verify", False)
     )
-    print("deployed abi token at {}".format(token.address))
-    return token
+    print("deployed {} at {}".format(contact_container._name, contract.address))
+    return contract
+
+def _deploy():
+    token_contract = deplopy_contract(AbiToken)
+    nft_contract = deplopy_contract(AbiMedal)
+    time_lock_contract = deplopy_contract(TimeLock, 3600, [], [])
+
+    abi_dao_contract = deplopy_contract(AbiDao, nft_contract, time_lock_contract)
 
 def main():
-    deploy()
+    _deploy()
--- tests/test_abi_token.py
+++ tests/test_abi_token.py
@@ -7,13 +7,13 @@ from web3 import Web3
 
 # from .tools import INITIAL_SUPPLE, get_account, LOCAL_BLOCKCHAIN
 
-from scripts.deploy import deploy
+from scripts.deploy import deplopy_contract
 from scripts.tools import LOCAL_BLOCKCHAIN, get_account
 
 def test_abi_token_supply():
     if network.show_active() not in LOCAL_BLOCKCHAIN:
         pytest.skip()
-    abi_token = deploy()
+    abi_token = deplopy_contract(AbiToken)
     initialSupply = 1000000
     contractOwner = abi_token.contractOwner()
     # print(abi_token.balanceOf(contractOwner))
