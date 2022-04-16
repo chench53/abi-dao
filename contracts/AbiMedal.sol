62bd9afa729b4d08395cdd21ba706b8a6e97d5ea
chench53 <chench53.logic@gmail.com>
2022-04-17 00:32:30 +0800

    implement erc721vote

--- contracts/AbiMedal.sol
+++ contracts/AbiMedal.sol
@@ -4,14 +4,16 @@ pragma solidity ^0.8;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/draft-ERC721Votes.sol"; // openzeppelin@v4.5.0 draft
+import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
 
 
-contract AbiMedal is ERC721URIStorage {
+contract AbiMedal is  ERC721URIStorage, ERC721Burnable,  EIP712, ERC721Votes {
 
     uint256 public initialSupply;
     uint256 public tokenCounter;
 
-    constructor () ERC721("AbiMedal", "ABIMEDAL") {
+    constructor() ERC721("AbiMedal", "ABI") EIP712("AbiMedal", "1") {
         tokenCounter = 0;
         initialSupply = 21;
     }
@@ -46,4 +48,25 @@ contract AbiMedal is ERC721URIStorage {
         super._beforeTokenTransfer(from, to, tokenId);
     }
 
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function _afterTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Votes)
+    {
+        super._afterTokenTransfer(from, to, tokenId);
+    }
 }
